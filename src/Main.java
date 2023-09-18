9cec1a697852bdd453ffd8af6779b9d475e9fdcc
RichAlien23 <anatoli.sein@gmail.com>
2023-09-18 18:19:31 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Say your name");
+        String name = scanner.next();
+
+        System.out.println("Welcome," + name +"!");
+
     }
 }
